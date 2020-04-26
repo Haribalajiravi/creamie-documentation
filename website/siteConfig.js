5726fd7bc352c8b741388b423247b11a1744879e
Haribalajiravi <haribalaji@examly.in>
2020-04-26 19:58:32 +0530

    Plugins usage added

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -85,7 +85,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'dark',
+    theme: 'ocean',
   },
 
   gaTrackingId: 'UA-162464001-2',
